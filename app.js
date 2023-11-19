acf7d865a27b8431cd21d78115725a1cbdabec37
karolina.czyzewska89@gmail.com <karlina.czyzewska89@gmail.com>
2023-11-19 17:16:10 +0100

    Zmiana1

---
+++ app.js
@@ -0,0 +1,9 @@
+
+// console.log("test");
+
+const header = document.querySelector(".header");
+header.classList.toggle("is-sticky", window.scrollY > 100);
+
+window.onscroll = () => {
+    header.classList.toggle("is-sticky", window.scrollY > 100);
+}
